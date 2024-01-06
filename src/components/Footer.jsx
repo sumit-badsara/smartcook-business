06a51a0059484f94492bd986f3f5c2a6ac913df5
Sumit Badsara <sbadsara@abnormalsecurity.com>
2024-01-07 01:15:07 +0530

    Remove social media

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -44,7 +44,7 @@ const Footer = () => {
         <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
           2024 SmartCook. All Rights Reserved.
         </p>  
-        <div className='flex flex-row md:mt-0 mt-6'>
+        {/* <div className='flex flex-row md:mt-0 mt-6'>
           {socialMedia.map((social, index) => (
             <img
               src={social.icon}
@@ -53,7 +53,7 @@ const Footer = () => {
               className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
             />
           ))}
-        </div>    
+        </div>     */}
       </div>
     </section>
   )
